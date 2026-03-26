52d3224229c886285324830088936efeda527ef5
lukas <s_posselt23@stud.hwr.berlin.de>
2026-03-26 23:43:40 +0100

    version 1.1

--- Dashboard/story.py
+++ Dashboard/story.py
@@ -942,7 +942,7 @@ elif statistik_view == "Spieler-Scouting":
 st.markdown("---")
 st.markdown(
     "<div style='text-align: center; color: gray;'>"
-    "<small>Powered by Team JALT - Deployed im Rahmen der LV Business Intelligence der HWR Berlin </small>"
+    "<small>Powered by Team JALT - Deployed im Rahmen der LV 'Business Intelligence' an der HWR Berlin </small>"
     "</div>",
     unsafe_allow_html=True
 )
